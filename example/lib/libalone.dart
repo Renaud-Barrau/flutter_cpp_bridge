a193bd808ab864c7e107864871c7c75207f67cdf
Renaud Barrau <rbarrau33@gmail.com>
2026-02-25 10:54:01 +0100

    style: format Dart source code
    Signed-off-by: Renaud Barrau <rbarrau33@gmail.com>

--- example/lib/libalone.dart
+++ example/lib/libalone.dart
@@ -2,13 +2,11 @@ import 'dart:ffi';
 
 import 'package:flutter_cpp_bridge/standalone_service.dart';
 
-
-class AloneService extends StandaloneService
-{
-  AloneService(super.libname)
-  {
-    hello = lib.lookup<NativeFunction<Void Function()>>('hello')
-      .asFunction<void Function()>();
+class AloneService extends StandaloneService {
+  AloneService(super.libname) {
+    hello = lib
+        .lookup<NativeFunction<Void Function()>>('hello')
+        .asFunction<void Function()>();
   }
 
   late Function hello;
